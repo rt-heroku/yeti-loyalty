3e002e80f9e02fce2b5eb90a53e7933449c36979
rt-heroku <rtorres@heroku.com>
2016-09-21 18:16:41 -0400

    make it better

---
+++ init_yeti.sql
@@ -0,0 +1,297 @@
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  1,
+                  'Yeti ICE',
+                  'YETI® ICE is filled to the brim with science, dialed in to the most effective temperature to maximize the ice retention of any cooler, with a durable design that is break-resistant. Its custom shape reduces freezing time and multiple size options mean that you can outfit everything from your Hopper™ 30 to your Tundra® 350 with YETI ICE. It works as an ice substitute or welcome addition to your ice stash to make sure your contents stay colder for longer. treats from the popular new boutique chocolatier in San Francisco''s Mission District.',
+                  'http://media.yeti.com/media/catalog/product/cache/1/small_image/400x295/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti-ice-1107x1107.jpg',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  4,
+                  'Tundra seat cushion in camo max 4',
+                  'Make your YETI® do double duty in the blind or dove field with the YETI Cooler Seat Cushion in Camo Max 4. To install, screw the included studs into the cooler lid and snap the cushion in place. Cushion is available for all YETI Tundra® models and all hardware is stainless steel.',
+                  'http://media.yeti.com/media/catalog/product/cache/1/small_image/400x295/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti-camo-cushion-1107x1107_1.jpg',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  2,
+                  'Bear Proof Locks',
+                  'The YETI Tundra® and Roadie® meet the Interagency Grizzly Bear Committee (IGBC) standards for bear-resistant containers when used with extra-long shank Master locks. Our hard coolers have been thoroughly tested in both controlled bear simulations and with wild grizzly bears. When padlocks are used properly, grizzlies get nothing but frustrated after coming across your cooler.',
+                  'http://media.yeti.com/media/catalog/product/cache/1/square_image/295x295/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti_accessories_1107x1107_bearprooflocks.jpg',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  3,
+                  'Locking Bracket',
+                  'Our YETI® stainless steel Locking Bracket slips into Tundra®’s AnchorPoint™ Tie-Down Slots and allows you to use a wide variety of locks, chains, or other security devices to keep your cooler secure. It’s perfect for locking your Tundra into a truck bed, trailer, or any other areas where your YETI might be vulnerable.',
+                  'http://media.yeti.com/media/catalog/product/cache/1/square_image/295x295/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti_accessories_1107x1107_lockingbracket.jpg',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  5,
+                  'Tundra Roadie',
+                  'The YETI® Roadie® is BYOB’s new best friend. Its ColdLock™ Gasket is built just like the Tundra®’s, so ice stays colder longer, and its No Sweat™ Design means you’ll never be stuck mopping up a mess. With its compact body and rotomolded construction, this small but mighty cooler is ready to hit the road, whether you’re in for a day of tailgating or kicking back at the ranch.',
+                  'http://media.yeti.com/media/catalog/product/cache/1/thumbnail/1500x879/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti-roadie-white-1.jpg',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  6,
+                  '30oz Tumbler',
+                  'Days on the dock are made better with the Rambler™ 30 oz. Tumbler. It’ll keep your dinnertime drink cool well past sundown or your coffee nice and hot throughout the morning. The Rambler 30 oz. Tumbler is made of 18/8 kitchen-grade stainless steel, has double-wall vacuum insulation, and No Sweat™ Design. Which means that you’ve got one heavy-duty, over-engineered cup on your hands with the Rambler Tumbler. It’s dishwasher safe, BPA-free, and ready for just about anything. Dishwasher-safe, comes with one Rambler Lid.',
+                  'http://media.yeti.com/media/wysiwyg/modules/16565-Product-Package-Modules-_sm-Rambler-Tumbler30.png',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  7,
+                  'Traditional Trucker Hat',
+                  'Our Traditional Trucker Hat marks your passion for hunting and fishing. A six-panel standard fit with a reinforced crown has a ventilated mesh back to cool you on a hot day offshore. A conversation starter, shade provider, and all-around badass, the Traditional Trucker is the gear needed for any member of the YETI® Nation.',
+                  'http://media.yeti.com/media/wysiwyg/modules/YETI-Trucker-Brown-189x189.png',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.product2 (id,
+                                 "name",
+                                 description,
+                                 image__c,
+                                 productpage__c,
+                                 publishdate__c,
+                                 family)
+        VALUES (
+                  8,
+                  'Tank',
+                  'Tank description',
+                  'http://media.yeti.com/media/wysiwyg/plp-cms/tank/tank-product-sketch.png',
+                  NULL,
+                  NULL,
+                  'Yeti');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  7,
+                  NULL,
+                  'Collegiate Tundras',
+                  NULL,
+                  NULL,
+                  'Gameday isn''t complete without cold drinks and a big show of school spirit. Get a Roadie, Tundra 45 or Tundra 65 with your school’s officially licensed logo',
+                  'http://media.yeti.com/media/wysiwyg/modules/custom-logos.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  6,
+                  NULL,
+                  'Free Shipping on Tank 45',
+                  NULL,
+                  NULL,
+                  'Free Shipping on all wine, champagne, and chocolate pairings.',
+                  'http://media.yeti.com/media/wysiwyg/modules/16565-Product-Package-Modules-Tank45T.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  5,
+                  NULL,
+                  'Free Shipping on Tundra 50',
+                  NULL,
+                  NULL,
+                  'Free Shipping for all Mother’s Day gifts places >72 hours before Sunday, May 11',
+                  'http://media.yeti.com/media/wysiwyg/modules/product_package/16747_YETI_Tundra_189x189_50_Blue.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  4,
+                  NULL,
+                  'Buy 4 Get 5: 20oz Tumbler',
+                  NULL,
+                  NULL,
+                  'Buy 4, get 6 of of the city''s finest native chocolatiers, old and new: Tcho, Dandelion, Recchiutti, Ghirardelli, Sharffenburger, Guittard.',
+                  'http://media.yeti.com/media/wysiwyg/modules/product_package/16565-Product-Package-Modules-RamblerTumbler20.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  3,
+                  NULL,
+                  'Buy 2 Get 1 Free: Ice packs',
+                  NULL,
+                  NULL,
+                  'Purists, Foodies and Afficionados: Buy 2 Bars Get 1 Free',
+                  'http://media.yeti.com/media/wysiwyg/modules/16565-Product-Package-Modules-_sm-MatrixIce-2lb.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  2,
+                  NULL,
+                  '10% off Tundra: Accessories',
+                  NULL,
+                  NULL,
+                  '0% off chocolate that makes you feel as good as they tastes! Fair Trade and Organic chocolates assortment...',
+                  'http://media.yeti.com/media/catalog/product/cache/1/square_image/295x295/9df78eab33525d08d6e5fb8d27136e95/y/e/yeti_accessories_1107x1107_mollezinger.jpg',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+INSERT INTO salesforce.campaign (id,
+                                 sfid,
+                                 "name",
+                                 startdate,
+                                 enddate,
+                                 description,
+                                 image__c,
+                                 campaignpage__c,
+                                 publishdate__c,
+                                 type,
+                                 status)
+        VALUES (
+                  1,
+                  NULL,
+                  '10% of Tundra',
+                  NULL,
+                  NULL,
+                  'Twice as much Eco!',
+                  'http://yeti.com/media/wysiwyg/plp-cms/tundra/YETI_Tundra_350.png',
+                  NULL,
+                  NULL,
+                  'Offer',
+                  'In Progress');
+
+COMMIT;
